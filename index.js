5058535ff73204c6d938203d63c8872951ac812e
Alexander Smith <ajsmitty89@gmail.com>
2018-05-14 13:01:35 -0700

    added css styles, modified js

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-
 const getDataFromApi = (searchTerm, callback) => {
 	const query = {
-		q: `${searchTerm} 
-		in:name`,
-		per_page: 5
-	}
-
-	$.getJSON(YOUTUBE_SEARCH_URL, query, callback);
-} 
-
-const renderResult = (result) => {
-	return `
-		<div>
-			<h2>
-			<a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}></a> by
-			<a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
-			<p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
-			<p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
-		</div>
-	`;	
-}
-const displayYouTubeSearchData = (data) => {
-	const results = data.items.map((item, index) =>
-		renderResult(item));
-		$('.js-search-results').html(results);
-}
-function watchSumbit() {
-	$('.js-search-form').submit(event => {
-		event.preventDefault();
-		const queryTarget = $(even.currentTarget).find('.js-query');
-		const query = queryTarget.val();
-		//clear out the input
-		queryTarget.val("");
-		getDataFromApi(query, displayYouTubeSearchData);
-	});
-}
-
-$(watchSumbit);
-
-const params = {
+	q: `${searchTerm} in:name`,
 	part: 'snippet',
-	key: 'AIzaSyDNqOlGbJFs95YPIab0ln69BmcmNrcWaY8',
-	q: `${}`
+	key: 'AIzaSyDNqOlGbJFs95YPIab0ln69BmcmNrcWaY8', 
+	per_page: 5
+	}
+	$.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+};
+function generateVideoElement(item) {
+    return `<div> 
+        <h2 class="js-return-title"> ${item.snippet.title} </h2>
+        <a href="https://www.youtube.com/watch?v=${item.id.videoId}"> 
+            <img src="${item.snippet.thumbnails.default.url}"></img>
+        </a>
+        <a href="https://www.youtube.com/channel/${item.snippet.channelId}">
+        	<p>${item.snippet.channelTitle}</p>
+    	</a>
+    </div>`
 }
+function generateChannelElement(item) {
+	return `<div>
+	 <a href="https://www.youtube.com/watch?v=${item.snippet.channelTitle}"></a>
+	</div>`;
+}
+function handleResponse(response) {
+	console.log(response);
+	let html = response.items.map(generateVideoElement).join('');
+	$('.js-search-results').html(html);
+
+}
+
+const onReady = () => {
+	$('form').submit((event) => {
+		event.preventDefault();
+		let input = $('#js-query').val();
+		console.log(input);
+	getDataFromApi(input, handleResponse);
+	});
+};
+
+
+
+$(onReady);
+
